df8c79f849c9f0bfc534229b7a6c6f6bd38d3793
Kaworu777 <144795359+Kaworu777@users.noreply.github.com>
2023-12-05 14:28:21 +0200

    dgfg
    fgsdgd

---
+++ lab4_taask2.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+def multi_func(n):
+    mult = 1
+    for element in n:
+        mult = mult * element
+        print(mult)
+    
+
+
+L = [100, 96, 73, 24, 56]
+list = np.array(L)
+    
---
+++ lab4_task1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def avg(n):
+    a = np.average(n)
+    print(a)
+
+L = [100, 96, 73, 24, 56]
+list = np.array(L)
+avg(list)
+
+
