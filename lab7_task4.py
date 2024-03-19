6a7b92bbbc7ab0b253e8fe67f808e814c5354ae5
Kaworu777 <144795359+Kaworu777@users.noreply.github.com>
2024-03-19 14:06:38 +0200

    3213312

--- lab7_task4.py
+++ lab7_task4.py
@@ -2,31 +2,33 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
-xdata, ydata = [], []
+xdata1 = []
+ydata1 = []
+D = 0.33
+C = 0.3
+x0 = 0.01
+y0 = 0.01
+xdata1.append(x0)
+ydata1.append(y0)
 
-def animate():
-    x0 = 0.1
-    y0 = 0.1
-    C = 0.3
-    D = 0.33
-    xdata.append(x0 ** 2 - y0 ** 2 + C)
-    ydata.append(2 * x0 * y0 + D)
-    fraq.set_data(xdata, ydata)
-    return fraq
 
-if __name__ == '__main__':
+def ggwp(i):
+    xdata1.append(xdata1[i-1]**2 - ydata1[i-1]**2 + C)
+    ydata1.append(2 * xdata1[i-1] * ydata1[i-1] + D) 
+    kill.set_data(xdata1, ydata1)
 
+if __name__ == "__main__":
+    plt.axis("equal")
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], 'o', color='r', label='Ball')
-
-    edge = 3
-    plt.axis('equal')
+    kill, = plt.plot([], [], "o", color="r", label="Ball")
+    edge = 1 
     ax.set_xlim(-edge, edge)
     ax.set_ylim(-edge, edge)
 
     ani = FuncAnimation(fig,
-                        animate,
-                        frames=180,
-                        interval=30)
+                            ggwp,
+                            frames=range(1, 50),
+                            interval=200)
+    ani.save("animation_4.gif")
 
-    ani.save('animation_4.gif') 
+
